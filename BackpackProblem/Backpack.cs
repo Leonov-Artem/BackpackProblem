cdd15fea1eda02088c4876115b83aeed6ab06c39
Leonov Artem <leonovru@yandex.ru>
2019-01-24 21:29:11 +0300

    Add class constructor

--- BackpackProblem/Backpack.cs
+++ BackpackProblem/Backpack.cs
@@ -8,6 +8,15 @@ namespace BackpackProblem
 {
     class Backpack<T> : IBackpack<T> where T : IComparable
     {
+        private T[] objects;
+        private T target_weight;
+        private List<T> solution;
 
+        public Backpack(T[] objects, T target_weight)
+        {
+            this.objects = object;
+            this.target_weight = target_weight;
+            solution = new List<T>();
+        }
     }
 }
