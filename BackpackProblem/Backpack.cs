0e428ff6579f2863a9205afc46a462839fb9bc87
Leonov Artem <leonovru@yandex.ru>
2019-01-24 23:53:59 +0300

    Add method realisation 'Solution' and tests in Program.cs

--- BackpackProblem/Backpack.cs
+++ BackpackProblem/Backpack.cs
@@ -14,9 +14,69 @@ namespace BackpackProblem
 
         public Backpack(T[] objects, T target_weight)
         {
-            this.objects = object;
+            this.objects = objects;
             this.target_weight = target_weight;
             solution = new List<T>();
+        }
+
+        /// <summary>
+        /// Поиск решения перебором всех объектов
+        /// </summary>
+        /// <returns></returns>
+        public List<T> Solution()
+        {
+            List<T> backpack;
+            for (int i = 0; i < objects.Length; i++)
+            {
+                backpack = GetSolutionForObject(i);
+                if (backpack.Count != 0)
+                    return backpack;
+            }
+            return new List<T>();
+        }
+        /// <summary>
+        /// Поиск решение для конкретного объекта из рюкзака
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        private List<T> GetSolutionForObject(int i)
+        {
+            solution.Add(objects[i]);
+            if (SolutionProblem(target_weight, objects[i]))
+                return solution;
+            else
+            {
+                solution.Remove(objects[i]);
+                return new List<T>();
+            }
+        }
+        /// <summary>
+        /// Поиска решения алгоритмом с возвратом
+        /// </summary>
+        /// <param name="target_weight"></param>
+        /// <param name="current_weight"></param>
+        /// <returns></returns>
+        private bool SolutionProblem(T target_weight, T current_weight)
+        {
+            if (target_weight.CompareTo(current_weight) < 0) return false;              // проверяем тестовое решение
+            if (target_weight.CompareTo(current_weight) == 0) return true;              // проверяем, является ли это решение полным
+
+            // расширяем частичное решение 
+            for (int i = 0; i < objects.Length; i++)
+            {
+                if (!solution.Contains(objects[i]))
+                {
+                    solution.Add(objects[i]);
+                    var target = target_weight as dynamic;
+                    var current = current_weight as dynamic;
+
+                    if (SolutionProblem(target - current, objects[i])) return true;     // проверяем частичное решение на допустимость
+                    solution.Remove(objects[i]);                                        // если расширение не ведет к решению, то отменяем расширение
+                }
+            }
+
+            //  если мы дошли до этой строки, то решения нет
+            return false;
         }
     }
 }
--- BackpackProblem/Program.cs
+++ BackpackProblem/Program.cs
@@ -10,7 +10,36 @@ namespace BackpackProblem
     {
         static void Main(string[] args)
         {
+            int weight = 16;
+            int[] objects = new int[] { 11, 4, 1, 5 };
 
+            Backpack<int> backpack = new Backpack<int>(objects, weight);
+            List<int> solution = backpack.Solution();
+
+            if (solution.Count != 0)
+            {
+                Console.Write($"Чтобы собрать рюкзак массой {weight} из объектов ");    Print(objects);
+                Console.Write($" необходимо взять ");                                   Print(solution);
+            }
+            else
+            {
+                Console.Write($"Из объектов ");
+                Print(objects);
+                Console.Write($" нельзя собрать рюкзак массой {weight}");
+            }
+
+            Console.ReadKey();
+        }
+        static void Print(int[] array)
+        {
+            foreach (var num in array)
+                Console.Write(num + " ");
+        }
+        static void Print(List<int> list)
+        {
+            foreach (var elem in list)
+                Console.Write(elem + " ");
+            Console.WriteLine();
         }
     }
 }
